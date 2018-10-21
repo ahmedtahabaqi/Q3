63b4d529215272527251e840e6ce8d9b88fa9687
ahmed taha <ahmedali1990128@yahoo.com>
2018-10-21 08:35:23 +0300

    imported project

---
+++ Q3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isheader = true;
+            var reader = new StreamReader(File.OpenRead(@"C:\Users\ahmed\Desktop\csvv.csv"));
+            List<string> headers = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                if (isheader)
+                {
+                    isheader = false;
+                    headers = values.ToList();
+                   
+                }
+                else
+                {
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        Console.Write(string.Format("{0} = {1};", headers[i], values[i]));                        
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
